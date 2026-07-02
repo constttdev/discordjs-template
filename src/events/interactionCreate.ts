f899c2e93e5df509a5e80fe996887ff90a070745
constt <175602385+constttdev@users.noreply.github.com>
2026-07-02 20:53:43 +0200

    feat: inital commit

--- src/events/interactionCreate.ts
+++ src/events/interactionCreate.ts
@@ -1,21 +1,62 @@
-import { Events } from "discord.js";
+import {
+  Events,
+  Interaction,
+  ChatInputCommandInteraction,
+  ButtonInteraction,
+  ModalSubmitInteraction,
+} from "discord.js";
+
+import * as ticketManager from "../utils/ticketManager";
+import * as todoManager from "../utils/todoManager";
 
 export const name = Events.InteractionCreate;
 
-export const execute = async (interaction) => {
-  if (!interaction.isChatInputCommand()) return;
-
-  const command = interaction.client.commands.get(interaction.commandName);
-
-  if (!command) {
-    console.error(`No command matching ${interaction.commandName} was found.`);
-    return;
-  }
-
+export const execute = async (interaction: Interaction) => {
   try {
-    await command.execute(interaction);
-  } catch (error) {
-    console.error(`Error executing ${interaction.commandName}`);
-    console.error(error);
+    if (interaction.isChatInputCommand()) {
+      const cmd = interaction.client.commands.get(interaction.commandName);
+      if (!cmd) return;
+      await cmd.execute(interaction as ChatInputCommandInteraction);
+      return;
+    }
+
+    if (interaction.isButton()) {
+      const btn = interaction as ButtonInteraction;
+
+      if (btn.customId.startsWith("todo_")) {
+        await todoManager.handleTodoButton(btn);
+        return;
+      }
+
+      await ticketManager.handleButton(btn);
+      return;
+    }
+
+    if (interaction.isModalSubmit()) {
+      const modal = interaction as ModalSubmitInteraction;
+
+      if (modal.customId.startsWith("todo_")) {
+        await todoManager.handleTodoModal(modal);
+        return;
+      }
+
+      await ticketManager.handleModal(modal);
+      return;
+    }
+  } catch (err) {
+    console.error(err);
+
+    if (
+      interaction.isChatInputCommand() ||
+      interaction.isButton() ||
+      interaction.isModalSubmit()
+    ) {
+      if (!interaction.replied && !interaction.deferred) {
+        await interaction.reply({
+          content: "Error processing interaction",
+          ephemeral: true,
+        });
+      }
+    }
   }
 };
--- src/events/ready.ts
+++ src/events/ready.ts
@@ -4,7 +4,7 @@ export const name = Events.ClientReady;
 
 export const execute = async (client) => {
   console.log(`Logged in as ${client.user.tag}`);
-  // client.user.setActivity("Example Status", {
-  //   type: ActivityType.Playing,
-  // });
+  client.user.setActivity("nyraclient.eu", {
+    type: ActivityType.Playing,
+  });
 };
